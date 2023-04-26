45393396343f1d82ced7eaf90ff31a79639c714f
merwin mathew <merwinmathew3@gmail.com>
2023-04-26 23:28:53 +0530

    fixed get orders

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, jsonify, request
+from flask import Flask,render_template, jsonify, request, redirect
 from pymongo import MongoClient
 from datetime import datetime
 
@@ -26,57 +26,81 @@ def get_all_users():
     users = list(users_collection.find({}, {'_id': 0}))
     return render_template('all_users.html', users=users)
 
+@app.route('/get_user_details', methods=['GET', 'POST'])
+def get_user_details():
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        return redirect('/users/'+user_id)
+    else:
+        return render_template('getu.html')
+
 
 @app.route('/users/<string:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
     # Get a specific user by id from the users collection
-    user = users_collection.find_one({'id': user_id}, {'_id': 0})
+    user = users_collection.find_one({'id': user_id}, {'id': 0})
     if user is None:
         return render_template('user_details.html', user=None)
     else:
         return render_template('user_details.html', user=user)
 
 
+
+@app.route('/get_order_details', methods=['GET', 'POST'])
+def get_order_details():
+    if request.method == 'POST':
+        order_id = request.form['order_id']
+        return redirect('/porders/'+order_id)
+    else:
+        return render_template('geto.html')
+
+
 @app.route('/orders', methods=['GET'])
 def get_all_orders():
     # Get all orders from the orders collection
-    orders = list(orders_collection.find({}, {'_id': 0}))
+    orders = list(orders_collection.find())
     return render_template('orders.html', orders=orders)
 
-@app.route('/orders/<string:order_id>', methods=['GET'])
+@app.route('/porders/<string:order_id>', methods=['GET'])
 def get_order_by_id(order_id):
     # Get a specific order by id from the orders collection
     order = orders_collection.find_one({'id': order_id}, {'_id': 0})
     if order is None:
         return jsonify({'error': 'Order not found'}), 404
-    return jsonify(order)
+    return render_template('order_details.html', order=order)
 
-@app.route('/ausers', methods=['POST'])
+
+@app.route('/ausers', methods=['GET', 'POST'])
 def add_user():
-    # Add a new user to the users collection
-    user_data = request.get_json()
-    user = {
-        'id': user_data['id'],
-        'name': user_data['name'],
-        'email': user_data['email'],
-        'created_at': datetime.now()
-    }
-    result = users_collection.insert_one(user)
-    return jsonify({'message': 'User added successfully', 'id': str(result.inserted_id)})
-@app.route('/orders', methods=['POST'])
+    if request.method == 'POST':
+        # Add a new user to the users collection
+        user_data = {
+            'id': request.form['id'],
+            'name': request.form['name'],
+            'email': request.form['email'],
+            'created_at': datetime.now()
+        }
+        result = users_collection.insert_one(user_data)
+        return render_template('index.html')
+    else:
+        return render_template('add_users.html')
+
+@app.route('/aorders', methods=['POST','GET'])
 def add_order():
-    # Add a new order to the orders collection
-    order_data = request.get_json()
-    order = {
-        'id': order_data['id'],
-        'user_id': order_data['user_id'],
-        'product_name': order_data['product_name'],
-        'quantity': order_data['quantity'],
-        'total_price': order_data['total_price'],
-        'created_at': datetime.now()
-    }
-    result = orders_collection.insert_one(order)
-    return jsonify({'message': 'Order added successfully', 'id': str(result.inserted_id)})
+    if request.method == 'POST':
+        # Add a new order to the orders collection
+        order_data = {
+            'id': request.form['id'],
+            'user_id': request.form['user_id'],
+            'product_name': request.form['product_name'],
+            'quantity': request.form['quantity'],
+            'total_price': request.form['total_price'],
+            'created_at': datetime.now()
+        }
+        result = orders_collection.insert_one(order_data)
+        return render_template('index.html')
+    else:
+        return render_template('add_orders.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
